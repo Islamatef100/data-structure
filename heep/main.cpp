191bf52fbb81a155be3ed43dba4230c79c518497
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 18:00:37 +0200

    Add files via upload

---
+++ heep/main.cpp
@@ -0,0 +1,16 @@
+//  heap with mustafa saad.
+#include <iostream>
+#include "heep.h"
+using namespace std;
+int main()
+{
+    heep_with_mustafa_saad h;
+    h.push(1);
+    h.push(13);
+    h.push(5);
+    h.print();
+    h.pop();
+    h.print();
+
+    return 0;
+}
